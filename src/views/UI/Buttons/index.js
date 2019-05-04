fb0995cb9a3b5d143eaac36a09a49d35d093287d
zhihua.ju <zhihua.ju@hand-china.com>
2019-05-04 10:53:08 +0800

    完成按钮和弹框界面开发

--- src/views/UI/Buttons/index.js
+++ src/views/UI/Buttons/index.js
@@ -1,12 +1,76 @@
 import React from 'react';
 import "./index.less";
+import { Card, Button, Radio } from 'antd';
 
-export default class Button extends React.Component {
+export default class Buttons extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: true,
+            size: 'default'
+        }
+    }
+
+    handCloseLoading = () => {
+        this.setState({
+            loading: false
+        });
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            size: e.target.value
+        });
+    }
+
     render() {
         return (
             <div className="button-warp">
-                this is button page
-      </div>
+                <Card title="基础按钮" className="card">
+                    <Button type="primary">Imooc</Button>
+                    <Button>Imooc</Button>
+                    <Button type="dashed">Imooc</Button>
+                    <Button type="danger">Imooc</Button>
+                    <Button disabled>Imooc</Button>
+                </Card>
+
+                <Card title="图形按钮" className="card">
+                    <Button icon="plus">创建</Button>
+                    <Button icon="edit">编辑</Button>
+                    <Button icon="delete">删除</Button>
+                    <Button shape="circle" icon="search"></Button>
+                    <Button type="primary" icon="search">搜索</Button>
+                    <Button type="primary" icon="download">下载</Button>
+                </Card>
+
+                <Card title="Loading按钮" className="card">
+                    <Button type="primary" loading={this.state.loading}>确定</Button>
+                    <Button type="primary" shape="circle" loading={this.state.loading}></Button>
+                    <Button loading={this.state.loading}>点击加载</Button>
+                    <Button shape="circle" loading={this.state.loading}></Button>
+                    <Button type="primary" onClick={this.handCloseLoading}>关闭</Button>
+                </Card>
+
+                <Card title="按钮组" className="card">
+                    <Button.Group className="button-group">
+                        <Button type="primary" icon="left">返回</Button>
+                        <Button type="primary" icon="right">前进</Button>
+                    </Button.Group>
+                </Card>
+
+                <Card title="基础按钮" className="card">
+                    <Radio.Group value={this.state.size} onChange={this.handleChange}>
+                        <Radio value="small">小</Radio>
+                        <Radio value="default">中</Radio>
+                        <Radio value="large">大</Radio>
+                    </Radio.Group>
+                    <Button type="primary" size={this.state.size}>Imooc</Button>
+                    <Button size={this.state.size}>Imooc</Button>
+                    <Button type="dashed" size={this.state.size}>Imooc</Button>
+                    <Button type="danger" size={this.state.size}>Imooc</Button>
+                </Card>
+            </div>
         )
     }
 }
--- src/views/UI/Modals/index.js
+++ src/views/UI/Modals/index.js
@@ -1,12 +1,95 @@
 import React from 'react';
+import { Card, Button, Modal } from "antd"
 import "./index.less";
 
 export default class Modals extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            showModal1: false,
+            showModal2: false,
+            showModal3: false,
+            showModal4: false,
+        }
+    }
+
+    handleOpen = (type) => {
+        this.setState({
+            [type]: true
+        });
+    }
+
+    handleCancel = (type) => {
+        this.setState({
+            [type]: false
+        })
+    }
+
+    handleConfirm = (type) => {
+        Modal[type]({
+            title: "确认",
+            content: '你确定你学会React了吗？',
+            onOk() {
+                console.log('Ok');
+            },
+            onCancel() {
+                console.log('Cancel');
+            }
+        })
+    }
+
     render() {
         return (
-            <div className="login-warp">
-                this is Modals page
-      </div>
+            <div className="modal-warp">
+                <Card title="基础模态框" className="card">
+                    <Button type="primary" onClick={() => this.handleOpen('showModal1')}>Open</Button>
+                    <Button type="primary" onClick={() => this.handleOpen('showModal2')}>自定义页脚</Button>
+                    <Button type="primary" onClick={() => this.handleOpen('showModal3')}>顶部20px弹框</Button>
+                    <Button type="primary" onClick={() => this.handleOpen('showModal4')}>水平垂直居中</Button>
+                </Card>
+                <Card title="信息确认框" className="card">
+                    <Button type="primary" onClick={() => this.handleConfirm('confirm')}>Confirm</Button>
+                    <Button type="primary" onClick={() => this.handleConfirm('info')}>Info</Button>
+                    <Button type="primary" onClick={() => this.handleConfirm('success')}>Success</Button>
+                    <Button type="primary" onClick={() => this.handleConfirm('warning')}>Warning</Button>
+                </Card>
+
+                <Modal
+                    title="React"
+                    visible={this.state.showModal1}
+                    onCancel={() => this.handleCancel('showModal1')}>
+                    <p>欢迎学习慕课新推出的React高级课程</p>
+                </Modal>
+
+                <Modal
+                    title="React"
+                    visible={this.state.showModal2}
+                    okText="好的"
+                    cancelText="算了"
+                    onCancel={() => this.handleCancel('showModal2')}
+                >
+                    <p>欢迎学习慕课新推出的React高级课程</p>
+                </Modal>
+
+                <Modal
+                    title="React"
+                    visible={this.state.showModal3}
+                    onCancel={() => this.handleCancel('showModal3')}
+                    style={{ top: 20 }}
+                >
+                    <p>欢迎学习慕课新推出的React高级课程</p>
+                </Modal>
+
+                <Modal
+                    title="React"
+                    visible={this.state.showModal4}
+                    wrapClassName="vertical-center-modal"
+                    onCancel={() => this.handleCancel('showModal4')}
+                >
+                    <p>欢迎学习慕课新推出的React高级课程</p>
+                </Modal>
+            </div>
         )
     }
 }
